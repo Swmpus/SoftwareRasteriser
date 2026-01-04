4b8e991da7958bb9711b37544f2a9a8cd60a7d34
Swmpus <morganshipsides@outlook.com>
2026-01-04 23:03:01 +0000

    Implemented a simple bitmap writer

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,73 @@
-#include <iostream>;
+#include <iostream>
+#include <fstream>
+#include <string>
 #include "maths.hpp"
+
+#define DISPLAY_HEIGHT 280
+#define DISPLAY_WIDTH 240
+
+void fileWriteBytes(std::ofstream& file, char* value, int length)
+{
+	file.write(value, length);
+}
+
+void fileWriteBytes(std::ofstream& file, uint32_t value)
+{
+	file.write(reinterpret_cast<const char*>(&value), 4);
+}
+
+void fileWriteBytes(std::ofstream& file, uint16_t value)
+{
+	file.write(reinterpret_cast<const char*>(&value), 2);
+}
+
+void fileWriteBytes(std::ofstream& file, uint8_t value)
+{
+	file.write(reinterpret_cast<const char*>(&value), 1);
+}
+
+void writeBitmap(Vec3 image[DISPLAY_HEIGHT][DISPLAY_WIDTH], std::string filePath)
+{
+	std::ofstream outputFile(filePath, std::ios::binary);
+	// BMP header
+	fileWriteBytes(outputFile, "BM", 2); // "BM" file header
+	fileWriteBytes(outputFile, static_cast<uint32_t>(14 + 40 +  4 * DISPLAY_HEIGHT * DISPLAY_WIDTH)); // File size in bytes (4 bytes allocated)
+	fileWriteBytes(outputFile, static_cast<uint32_t>(0));
+	fileWriteBytes(outputFile, static_cast<uint32_t>(14 + 40)); // Data start offset
+	// // DIB header
+	fileWriteBytes(outputFile, static_cast<uint32_t>(40)); // Size of the DIB header
+	fileWriteBytes(outputFile, static_cast<uint32_t>(DISPLAY_WIDTH));
+	fileWriteBytes(outputFile, static_cast<uint32_t>(DISPLAY_HEIGHT));
+	fileWriteBytes(outputFile, static_cast<uint16_t>(1)); // Number of colour planes? Not clear from wikipedia
+	fileWriteBytes(outputFile, static_cast<uint16_t>(8 * 4)); // Bits per pixel
+	fileWriteBytes(outputFile, static_cast<uint32_t>(0)); // Compression method (None)
+	fileWriteBytes(outputFile, static_cast<uint32_t>(4 * DISPLAY_HEIGHT * DISPLAY_WIDTH)); // Image size in bytes (can be 0)
+	fileWriteBytes(outputFile, static_cast<uint32_t>(0)); // Unused print/palette setting
+	fileWriteBytes(outputFile, static_cast<uint32_t>(0)); // Unused print/palette setting
+	fileWriteBytes(outputFile, static_cast<uint32_t>(0)); // Unused print/palette setting
+	fileWriteBytes(outputFile, static_cast<uint32_t>(0)); // Unused print/palette setting
+	// Data
+	for (uint16_t y = 0; y < DISPLAY_HEIGHT; y++) {
+		for (uint16_t x = 0; x < DISPLAY_WIDTH; x++) {
+			fileWriteBytes(outputFile, static_cast<uint8_t>(image[y][x].z * 255));
+			fileWriteBytes(outputFile, static_cast<uint8_t>(image[y][x].y * 255));
+			fileWriteBytes(outputFile, static_cast<uint8_t>(image[y][x].x * 255));
+			fileWriteBytes(outputFile, static_cast<uint8_t>(0)); // Padding
+		}
+	}
+	outputFile.close();
+}
 
 int main()
 {
-	const Vec3 a = Vec3(1, 2, 3);
-	std::cout << "(" << a.x << ", " << a.y << ", " << a.z  << ")" << std::endl;
+	Vec3 image[DISPLAY_HEIGHT][DISPLAY_WIDTH];
+
+	for (uint16_t y = 0; y < DISPLAY_HEIGHT; y++) {
+		for (uint16_t x = 0; x < DISPLAY_WIDTH; x++) {
+			image[y][x].x = static_cast<float>(x) / static_cast<float>(DISPLAY_WIDTH - 1);
+			image[y][x].y = static_cast<float>(y) / static_cast<float>(DISPLAY_HEIGHT - 1);
+			image[y][x].z = 0.0f;
+		}
+	}
+	writeBitmap(image, "D:/New Projects/Programming/SoftwareRasteriser/output.bmp");
 }
--- source/maths/maths.hpp
+++ source/maths/maths.hpp
@@ -7,4 +7,5 @@ struct Vec3 {
 	float z;
 
 	Vec3(float x, float y, float z) : x(x), y(y), z(z) {}
+	Vec3() : x(0.0f), y(0.0f), z(0.0f) {}
 };
